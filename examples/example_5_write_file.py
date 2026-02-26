f07140183bcd2a92742064353ef9ecd565f9dee1
ariannedee <ariannedee@gmail.com>
2026-02-25 16:34:05 -0800

    Add example 5

---
+++ examples/example_5_write_file.py
@@ -0,0 +1,19 @@
+lines = [
+    "I have eaten",
+    "the plums",
+    "that were in",
+    "the icebox",
+]
+
+# Simplest
+with open("data/output.txt", "w") as file:
+    for line in lines:
+        file.write(line + "\n")
+
+# With .join()
+with open("data/output.txt", "w") as file:
+    file.write("\n".join(lines))
+
+# With a generator expression or comprehension
+with open("data/output.txt", "w") as file:
+    file.writelines(line + "\n" for line in lines)
