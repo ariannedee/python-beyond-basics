c595fb321a7815e0cfa96f13620c08b6fad4076a
ariannedee <ariannedee@gmail.com>
2026-02-25 16:25:26 -0800

    Add example 4

---
+++ examples/example_4_read_file.py
@@ -0,0 +1,9 @@
+"""
+Read a file with a number on each line. Print the sum of those numbers.
+"""
+sum_ = 0
+with open("data/input.txt") as file:
+    for line in file.readlines():
+        sum_ += int(line)
+
+print(f"The sum is {sum_}")
