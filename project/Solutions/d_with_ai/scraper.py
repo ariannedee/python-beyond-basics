f694d6356296d7bbf8f7fd718bcedde7ab3e7b7d
ariannedee <ariannedee@gmail.com>
2026-06-29 14:03:13 -0700

    Update for June 2026 class

---
+++ project/Solutions/d_with_ai/scraper.py
@@ -0,0 +1,19 @@
+"""
+Documentation: https://github.com/ollama/ollama-python
+"""
+import ollama
+
+# Use generate() for one-shot prompts
+response = ollama.generate(prompt="Hello, world", model="llama3.2")
+print(response.response)
+
+# Use chat() for messages w/ context
+prompt = "Write a hello world script in python"
+
+messages = [{"role": "user", "content": prompt}]
+response: ollama.ChatResponse = ollama.chat(
+    model="llama3.2", messages=messages
+)
+
+print(response["message"]["content"])  # Access via keys
+print(response.message.content)        # Access via attributes
