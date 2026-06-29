eaabc1d17b6827178880ab33c1c70c9a73f9a724
ariannedee <ariannedee@gmail.com>
2026-06-29 13:56:40 -0700

    Update Ollama example to include generate()

--- examples/example_10_ollama.py
+++ examples/example_10_ollama.py
@@ -3,10 +3,16 @@ Documentation: https://github.com/ollama/ollama-python
 """
 import ollama
 
+# Use generate() for one-shot prompts
+response = ollama.generate(prompt="Hello, world", model="llama3.2")
+print(response.response)
+
+# Use chat() for messages w/ context
 prompt = "Write a hello world script in python"
 
+messages = [{"role": "user", "content": prompt}]
 response: ollama.ChatResponse = ollama.chat(
-    model="llama3", messages=[{"role": "user", "content": prompt}]
+    model="llama3", messages=messages
 )
 
 print(response["message"]["content"])  # Access via keys
